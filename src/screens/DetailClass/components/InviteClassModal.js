ea5a142849cc0e5b82474958dd7bf44dc41f4df2
Duy Tran <duyithcmus2016@gmail.com>
2021-11-23 01:05:29 +0700

    add UI invite class modal

---
+++ src/screens/DetailClass/components/InviteClassModal.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import nProgress from "nprogress";
+import { Modal, Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { Formik } from 'formik';
+import userService from "../../../services/user.service";
+import { createClass } from "../../../redux/actions/class.action";
+export default function InviteClassModal({ show, handleClose, isInviteTeacher }) {
+  const [teachers, setTeachers] = useState([]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    userService.getUserByRole('teacher')
+      .then(value => {
+        if (value.data) {
+          setTeachers(value.data)
+        }
+      })
+  }, [])
+
+  return (
+    <Modal
+      show={show}
+      onHide={handleClose}
+      backdrop="static"
+      keyboard={false}
+    >
+      <Formik
+        initialValues={{ email: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Field email is required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          nProgress.start();
+          const dataInvite = {
+            email: values.email
+          }
+          setTimeout(() => {
+            dispatch(createClass(dataInvite));
+            setSubmitting(true);
+            nProgress.done();
+            handleClose();
+          }, 500);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Form onSubmit={handleSubmit}>
+            <Modal.Header closeButton>
+              <Modal.Title>{isInviteTeacher ? "Mời giáo viên" : "Mời học viên"}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Form.Group className="mb-3">
+                <Form.Label>Nhập email</Form.Label>
+                <Form.Control type="text"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  placeholder="abcxyz@gmail.com" />
+                <Form.Text className="text-error">
+                  {(errors.name && touched.name)}
+                </Form.Text>
+              </Form.Group>
+            </Modal.Body>
+            <Modal.Footer>
+                <Form.Label className="h6 text-muted">
+                    {isInviteTeacher ? "Giáo viên bạn thêm có thể làm mọi thứ, trừ xóa lớp học." : "" }
+                </Form.Label>
+                <Button variant="outline-secondary"
+                    onClick={handleClose}>
+                    Đóng
+                </Button>
+                <Button variant="outline-success" type="submit" disabled={isSubmitting}>Mời</Button>
+            </Modal.Footer>
+          </Form>
+        )}
+      </Formik>
+    </Modal>
+  );
+}
