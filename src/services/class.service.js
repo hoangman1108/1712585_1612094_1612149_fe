dfabe05d420ebadc1fc173f9cbf810d3821c5c10
DatNC <datnc@younetgroup.com>
2021-12-01 00:25:37 +0700

    add api patch for updateIndexAssigments

--- src/services/class.service.js
+++ src/services/class.service.js
@@ -11,48 +11,65 @@ const detailClass = (id) => {
 };
 
 const checkUserInClass = (classId, userId) => {
-  return axios.get(API_URL + `/classes/${classId}/checkUser`, { headers: authHeader() });
-}
+  return axios.get(API_URL + `/classes/${classId}/checkUser`, {
+    headers: authHeader(),
+  });
+};
 
 const createClass = (data) => {
   return axios.post(API_URL + "/classes", data, { headers: authHeader() });
-}
+};
 
 const deleteClass = (id) => {
   return axios.delete(API_URL + `/classes/${id}`, { headers: authHeader() });
-}
+};
 
 const joinClass = (data) => {
-  return axios.post(API_URL + "/classes/joinClass", data, { headers: authHeader() });
-}
+  return axios.post(API_URL + "/classes/joinClass", data, {
+    headers: authHeader(),
+  });
+};
 
 const getAssigments = (classId) => {
-  return axios.get(API_URL + `/classes/${classId}/assignments`, { headers: authHeader() });
-}
+  return axios.get(API_URL + `/classes/${classId}/assignments`, {
+    headers: authHeader(),
+  });
+};
+
+const updateIndexAssigments = (classId, data) => {
+  return axios.patch(API_URL + `/classes/${classId}/assignments`, data, {
+    headers: authHeader(),
+  });
+};
 
 const updateAssigments = (data) => {
-  return axios.put(API_URL + `/assignments/${data.id}`, data, { headers: authHeader() });
-}
+  return axios.put(API_URL + `/assignments/${data.id}`, data, {
+    headers: authHeader(),
+  });
+};
 
 const deleteAssigments = (id) => {
-  return axios.delete(API_URL + `/assignments/${id}`, { headers: authHeader() });
-}
+  return axios.delete(API_URL + `/assignments/${id}`, {
+    headers: authHeader(),
+  });
+};
 
 const addAssigments = (data) => {
   return axios.post(API_URL + "/assignments", data, { headers: authHeader() });
-}
+};
 
 const classService = {
   getAllClass,
   deleteClass,
   createClass,
   joinClass,
+  updateIndexAssigments,
   detailClass,
   checkUserInClass,
   getAssigments,
   updateAssigments,
   deleteAssigments,
-  addAssigments
+  addAssigments,
 };
 
 export default classService;
