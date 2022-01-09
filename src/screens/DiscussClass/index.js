53dca53c016e3826589dac1c60d46e19de2008d5
DatNC <datnc@younetgroup.com>
2022-01-09 18:30:07 +0700

    add component ListPost in tab discussClass

--- src/screens/DiscussClass/index.js
+++ src/screens/DiscussClass/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { useSelector } from "react-redux";
 import ListPost from "./components/ListPost";
 import TabsDetail from "../ClassScreen/components/TabsDetail";
 
 export default function DiscussClass() {
+  const { me } = useSelector((state) => state.auth);
+  console.log("me ne", me);
   return (
     <Container>
       <TabsDetail />
-      <ListPost />
+      <ListPost me={me} />
     </Container>
   );
 }
