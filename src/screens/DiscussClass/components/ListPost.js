ef29274fb784462b63107eed3c5fa0546a375072
DatNC <datnc@younetgroup.com>
2022-01-09 18:30:42 +0700

    add method getGradeView for ListPost

--- src/screens/DiscussClass/components/ListPost.js
+++ src/screens/DiscussClass/components/ListPost.js
@@ -1,50 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 import { Accordion, Col, Card, Form, Button, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileContract } from "@fortawesome/free-solid-svg-icons";
-export default function According() {
+import classService from "../../../services/class.service";
+export default function ListPost(props) {
+  const { me } = props;
+  const history = useHistory();
+  const { name, role, id } = me;
+  const classID = history.location.pathname.split("/")[2];
+  const [posts, setPosts] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const res = await classService.getGradeView(classID);
+      const data = res?.data;
+      console.log("data ne", data);
+
+      setPosts(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
   return (
     <Row className="mt-5">
       <Col sm={{ span: 8, offset: 2 }}>
-        {/* defaultActiveKey="0" */}
         <Accordion>
-          <Accordion.Item eventKey="0" className="mt-3">
-            <Accordion.Header>
-              <FontAwesomeIcon icon={faFileContract} />
-              <span className="ps-3">Post</span>
-            </Accordion.Header>
-            <Accordion.Body className="shadow rounded">
-              <Card>
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  {/* <Button variant="primary">Go somewhere</Button> */}
-                </Card.Body>
-              </Card>
-              <br />
+          {posts.map((e, index) => (
+            <Accordion.Item key={index} eventKey="0" className="mt-3">
+              <Accordion.Header>
+                <FontAwesomeIcon icon={faFileContract} />
+                <span className="ps-3">{`${e.composition.name} - ${e.Mssv} `}</span>
+              </Accordion.Header>
+              <Accordion.Body className="shadow rounded">
+                <Card>
+                  {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                  <Card.Body>
+                    <Card.Title>{e.Mssv}</Card.Title>
+                    <Card.Text>
+                      <div>{`Current grade: ${e.current}`}</div>
+                      <div>{`Expect grade: ${e.expect}`}</div>
+                      <div>{`Explanation: ${e.explanation}`}</div>
+                    </Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                  </Card.Body>
+                </Card>
+                <br />
+                <h6>Comments: </h6>
+                <Card>
+                  {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+                  <Card.Body>
+                    <Card.Text>
+                      <span>1612094: </span>
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </Card.Text>
+                    {/* <Button variant="primary">Go somewhere</Button> */}
+                  </Card.Body>
+                </Card>
+                <br />
 
-              <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                  <Form.Label>Your comment</Form.Label>
-                  <Form.Control type="text" placeholder="new comment" />
-                  {/* <Form.Text className="text-muted">
-                    We'll never share your email with anyone else.
-                  </Form.Text> */}
-                </Form.Group>
+                <Form>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Your comment</Form.Label>
+                    <Form.Control type="text" placeholder="new comment" />
+                  </Form.Group>
 
-                {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                  <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group> */}
-                <Button variant="primary" type="submit">
-                  Create comment
-                </Button>
-              </Form>
-            </Accordion.Body>
-          </Accordion.Item>
+                  <Button variant="primary" type="submit">
+                    Create comment
+                  </Button>
+                </Form>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
         </Accordion>
       </Col>
     </Row>
